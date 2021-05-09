767e6ff3d14ff98ba074afe6104c700606d156de
Luisa <luisatrece@gmail.com>
2021-05-09 18:38:33 +0200

    Trip model complete

--- backend-ulysses/src/main/java/com/luisa13/backendulysses/model/Trip.java
+++ backend-ulysses/src/main/java/com/luisa13/backendulysses/model/Trip.java
@@ -1,6 +1,11 @@
 package com.luisa13.backendulysses.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.persistence.*;
 
 @Entity
@@ -11,13 +16,55 @@ public class Trip {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "idTrip")
 	Long id;
+	
 	@Column(name = "name")
 	String name;
+	
 	@Column(name = "date")
 	Date date;
 	
+	@ManyToMany(mappedBy = "trips")
+	private Set<User>users;
+	
+	@ManyToOne
+	private List<Stage>stages;
+	
 	public Trip(String name) {
 		this.name = name;
+		this.users = new HashSet<User>();
+		this.stages = new ArrayList<Stage>();
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Date getDate() {
+		return this.date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public List<Stage> getStages(){
+		return this.stages;
+	}
+	
+	public void setStages(List<Stage> stages) {
+		this.stages = stages;
 	}
 
 }
