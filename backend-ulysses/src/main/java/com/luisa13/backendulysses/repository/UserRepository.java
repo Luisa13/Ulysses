45db3b3e3c84af417551f77465d9e57d105dc01f
Luisa <luisatrece@gmail.com>
2021-05-09 18:32:38 +0200

    user repository added

---
+++ backend-ulysses/src/main/java/com/luisa13/backendulysses/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.luisa13.backendulysses.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luisa13.backendulysses.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	User findByName(String name);
+}
