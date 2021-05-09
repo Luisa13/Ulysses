af384532d02bd177e0328f6287daec8f315c4059
Luisa <luisatrece@gmail.com>
2021-05-09 18:32:04 +0200

    user model complete

--- backend-ulysses/src/main/java/com/luisa13/backendulysses/model/User.java
+++ backend-ulysses/src/main/java/com/luisa13/backendulysses/model/User.java
@@ -1,5 +1,8 @@
 package com.luisa13.backendulysses.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.*;
 
 @Entity
@@ -14,10 +17,45 @@ public class User {
 	private String name;
 	@Column(name = "surname")
 	private String surname;
+	@ManyToMany
+	Set<Trip> trips;
 	
 	public User(String name, String surname) {
 		this.name = name;
 		this.surname = surname;
+		this.trips = new HashSet<Trip>();
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name; 
+	}
+	
+	public String getSurname() {
+		return this.surname;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public Set<Trip> getTrips(){
+		return this.trips;
+	}
+	
+	public void setTrips(Set<Trip> trips) {
+		this.trips = trips;
+	}
+	
+	public void addTrip(Trip trip) {
+		this.trips.add(trip);
 	}
 	
 	
