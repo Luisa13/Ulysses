2bdd86ecb829212b3471aebd6617b5f0046212ac
Luisa <luisatrece@gmail.com>
2021-05-09 12:49:07 +0200

    re-init

---
+++ backend-ulysses/src/main/java/com/luisa13/backendulysses/BackendUlyssesApplication.java
@@ -0,0 +1,13 @@
+package com.luisa13.backendulysses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUlyssesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUlyssesApplication.class, args);
+	}
+
+}
---
+++ backend-ulysses/src/test/java/com/luisa13/backendulysses/BackendUlyssesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisa13.backendulysses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUlyssesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
