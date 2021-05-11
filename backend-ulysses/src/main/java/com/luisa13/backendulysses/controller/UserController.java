eb3863586fd08db469a579e24fc041c2baf606b2
Luisa <luisatrece@gmail.com>
2021-05-11 20:32:47 +0200

    UserController added

---
+++ backend-ulysses/src/main/java/com/luisa13/backendulysses/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.luisa13.backendulysses.controller;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.luisa13.backendulysses.model.User;
+import com.luisa13.backendulysses.service.UserService;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public User addUser(@RequestBody User user) {
+		return this.userService.addUSer(user);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteUser(@PathVariable Long id) {
+		this.userService.deleteUserById(id);
+	}
+	
+	@PutMapping("/{updateuser}")
+	public ResponseEntity<String> updateUSer(@RequestBody User user) {
+		HttpStatus response;
+		try {
+			this.userService.updateUser(user);
+			response = HttpStatus.OK;
+		}catch(NoSuchElementException ex) {
+			System.out.println(ex.getMessage());
+			response = HttpStatus.NOT_FOUND;
+		}
+		
+		return new ResponseEntity<String>(response);
+	}
+	
+	@GetMapping("/allusers")
+	public List<User> getAllUser(){
+		return this.userService.getAllUsers();
+	}
+	
+	
+	
+	
+}
