232f67285623f8d857b00cccf59feec73f10bdc4
Luisa <luisatrece@gmail.com>
2021-05-12 22:22:29 +0200

    User test added

---
+++ backend-ulysses/src/test/java/com/luisa13/backendulysses/UserTest.java
@@ -0,0 +1,74 @@
+package com.luisa13.backendulysses;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.print.ServiceUI;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.luisa13.backendulysses.model.User;
+import com.luisa13.backendulysses.service.UserService;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ComponentScan(basePackages = {"com.luisa13.backendulysses.service"})
+class UserTest {
+	
+	@Autowired
+	UserService userService;
+
+	@Test
+	void testSaveUser() {
+		User user = new User("Jhon", "Doe");
+		User dbUser = userService.addUSer(user);
+		
+		assertTrue(dbUser != null && dbUser.getId() == user.getId());
+	}
+	
+	@Test
+	void testgetUSerThatExists() {
+		User user = new User("Daniel", "Jackson");
+		userService.addUSer(user);
+		User foundUser = userService.findUserById(user.getId());
+		
+		assertTrue(foundUser.getId() == user.getId());
+	}
+	
+	@Test
+	void testGetUserNotExists() {
+		User foundUser = userService.findUserById(Integer.toUnsignedLong(1));
+		assertNull(foundUser);
+	}
+	
+	@Test
+	void testGetAllUsers() {
+		User user1 = new User("Jhon", "Doe");
+		this.userService.addUSer(user1);
+		User user2 = new User("Daniel", "Jackson");
+		this.userService.addUSer(user2);
+		
+		assertNotNull(this.userService.getAllUsers());
+	}
+	
+	@Test
+	void testDeleteUserThatExists() {
+		User user = new User("Jhon", "Doe");
+		this.userService.addUSer(user);
+		this.userService.deleteUserById(user.getId());
+		
+		assertTrue(this.userService.getAllUsers().isEmpty());
+	}
+	
+	
+	
+
+}
