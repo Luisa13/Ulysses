b606e602cbf25b611759bdec7c991befc56da7ad
Luisa <luisatrece@gmail.com>
2021-05-14 20:16:37 +0200

    [Fix Bug] Acces from local allowed

--- backend-ulysses/src/main/java/com/luisa13/backendulysses/controller/UserController.java
+++ backend-ulysses/src/main/java/com/luisa13/backendulysses/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.luisa13.backendulysses.service.UserService;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/user")
 public class UserController {
 
