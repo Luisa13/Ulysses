6ec89a4d0a38a1e9db15f84e0f988736f49429a0
Luisa <luisatrece@gmail.com>
2021-06-26 23:46:54 +0200

    Fix method parsing according to the field in the model

--- backend-ulysses/src/main/java/com/luisa13/backendulysses/repository/UserRepository.java
+++ backend-ulysses/src/main/java/com/luisa13/backendulysses/repository/UserRepository.java
@@ -3,11 +3,13 @@ package com.luisa13.backendulysses.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.luisa13.backendulysses.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
-	Optional<User> findByUserOrEmail(String usernameEmail, String usernameEmail2);
+	Optional<User> findByName(String username);
 
 }
