0bb4a9ad620ae97cef6ae4b9737c76459961f07e
Luisa <luisatrece@gmail.com>
2021-06-26 23:49:04 +0200

    Add configuration for security

--- backend-ulysses/src/main/java/com/luisa13/backendulysses/config/SecurityConfig.java
+++ backend-ulysses/src/main/java/com/luisa13/backendulysses/config/SecurityConfig.java
@@ -1,11 +1,63 @@
 package com.luisa13.backendulysses.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.BeanIds;
+
+import com.luisa13.backendulysses.security.CustomUserDetailService;
+import com.luisa13.backendulysses.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	CustomUserDetailService customUserDetailService;
 	
+	@Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder
+		.userDetailsService(this.customUserDetailService);
+	}
+	
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+	
+	@Override
+	 protected void configure (HttpSecurity http) throws Exception {
+	  http.csrf().disable()
+	          .authorizeRequests()
+	          .mvcMatchers( "/auth/**" ).hasAuthority( "ADMIN" )
+	          .mvcMatchers( "/**" ).permitAll()
+	          .and()
+	          .sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS )
+	          .and();
+	 http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+	          
+	 }
+
+	
+
 }
