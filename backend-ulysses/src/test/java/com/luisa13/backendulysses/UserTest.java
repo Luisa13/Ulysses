b95f7aeafdb2697a780d109c78615a167498a5e0
Luisa <luisatrece@gmail.com>
2021-06-26 23:47:30 +0200

    Updates test the users

--- backend-ulysses/src/test/java/com/luisa13/backendulysses/UserTest.java
+++ backend-ulysses/src/test/java/com/luisa13/backendulysses/UserTest.java
@@ -26,7 +26,7 @@ class UserTest {
 
 	@Test
 	void testSaveUser() {
-		User user = new User("Jhon", "Doe");
+		User user = new User("Jhon", "Doe", "jhon@doe.com");
 		User dbUser = userService.addUSer(user);
 		
 		assertTrue(dbUser != null && dbUser.getId() == user.getId());
@@ -34,7 +34,7 @@ class UserTest {
 	
 	@Test
 	void testgetUSerThatExists() {
-		User user = new User("Daniel", "Jackson");
+		User user = new User("Daniel", "Jackson", "daniel@jackson.com");
 		userService.addUSer(user);
 		User foundUser = userService.findUserById(user.getId());
 		
@@ -49,9 +49,9 @@ class UserTest {
 	
 	@Test
 	void testGetAllUsers() {
-		User user1 = new User("Jhon", "Doe");
+		User user1 = new User("Jhon", "Doe", "jhon@doe.com");
 		this.userService.addUSer(user1);
-		User user2 = new User("Daniel", "Jackson");
+		User user2 = new User("Daniel", "Jackson", "daniel@jackson.com");
 		this.userService.addUSer(user2);
 		
 		assertNotNull(this.userService.getAllUsers());
@@ -59,7 +59,7 @@ class UserTest {
 	
 	@Test
 	void testDeleteUserThatExists() {
-		User user = new User("Jhon", "Doe");
+		User user = new User("Jhon", "Doe",  "jhon@doe.com");
 		this.userService.addUSer(user);
 		this.userService.deleteUserById(user.getId());
 		
