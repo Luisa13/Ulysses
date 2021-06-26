2e7eb10bfccc6be5b3b031e6e3052278ece8e95d
Luisa <luisatrece@gmail.com>
2021-06-26 23:49:26 +0200

    Add authentication controller

---
+++ backend-ulysses/src/main/java/com/luisa13/backendulysses/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.luisa13.backendulysses.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luisa13.backendulysses.security.JwtAuthResponse;
+import com.luisa13.backendulysses.security.JwtLoginRequest;
+import com.luisa13.backendulysses.security.JwtTokenProvider;
+import com.luisa13.backendulysses.service.UserService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+	
+	@Autowired
+	AuthenticationManager authManager;
+	
+	//UserRepository userRepo;
+	@Autowired
+	UserService userService;
+	@Autowired
+	JwtTokenProvider tokenProvider;
+	
+	@PostMapping
+	public ResponseEntity<?> authenticateUser(@RequestBody JwtLoginRequest loginRequest){
+		Authentication authentication =  this.authManager.authenticate(new UsernamePasswordAuthenticationToken(
+				loginRequest.getUsername(), 
+				loginRequest.getPassword()));
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwtToken = this.tokenProvider.generateToken(authentication);
+		return ResponseEntity.ok(new JwtAuthResponse(jwtToken));
+	} 
+
+}
