28bb0c08addc145949f05a299ed9cbc6e5364195
Luisa <luisatrece@gmail.com>
2021-06-26 15:49:01 +0200

    Added new method to find users by username

--- backend-ulysses/src/main/java/com/luisa13/backendulysses/repository/UserRepository.java
+++ backend-ulysses/src/main/java/com/luisa13/backendulysses/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.luisa13.backendulysses.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luisa13.backendulysses.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+
+	Optional<User> findByUserOrEmail(String usernameEmail, String usernameEmail2);
 
 }
