e4af16abd0df4176d90e612fdda53e72fb94acde
Luisa <luisatrece@gmail.com>
2021-06-26 23:47:58 +0200

    Adds model for the request and response

---
+++ backend-ulysses/src/main/java/com/luisa13/backendulysses/security/JwtLoginRequest.java
@@ -0,0 +1,26 @@
+package com.luisa13.backendulysses.security;
+
+public class JwtLoginRequest {
+	
+	
+	private String username;
+	
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
