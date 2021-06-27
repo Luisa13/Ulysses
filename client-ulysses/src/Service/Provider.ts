5c75b761fbde6f3eee98e63cce3c06773025e836
Luisa <luisatrece@gmail.com>
2021-06-27 23:57:48 +0200

    Add method to provide stages

--- client-ulysses/src/Service/Provider.ts
+++ client-ulysses/src/Service/Provider.ts
@@ -1,9 +1,18 @@
 import UserService from "../domain/use-case/UserService";
 import UserRepository from "../domain/repository/UserRepository"
+import StageService from "../domain/use-case/StageService";
+import StageRepository from "../domain/repository/StageRepository";
 
 export function ProviderUsers(): UserService{
     const userRepository = new UserRepository();
     const userService = new UserService(userRepository);
 
     return userService;
+}
+
+export function ProviderStages(): StageService{
+    const stageRepository = new StageRepository();
+    const stageService = new StageService(stageRepository);
+
+    return stageService;
 }
