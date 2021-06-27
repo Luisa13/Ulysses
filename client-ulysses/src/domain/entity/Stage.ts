e6054fa0c0960cfbd5c2e8d5834d74f38a69ed31
Luisa <luisatrece@gmail.com>
2021-06-27 18:23:28 +0200

    Adds the stage model

---
+++ client-ulysses/src/domain/entity/Stage.ts
@@ -0,0 +1,26 @@
+export default class Stage{
+    id: number;
+    place: string;
+    startDate: Date;
+    endDate: Date;
+    //image: File;
+    description: string;
+
+    constructor(id: number, place: string, startDate: Date, endDate: Date){
+        this.id = id;
+        this.place = place;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = "";
+        //this.image ;
+    }
+
+   /* setImage(image: File){
+        this.image = image;
+    }*/
+
+    setDescription(escription: string){
+        this.description = this.description;
+    }
+
+}
