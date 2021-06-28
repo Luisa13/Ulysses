ed46ad268f9c7af2b0b2a42ca4bd4045c92a8eb5
Luisa <luisatrece@gmail.com>
2021-06-28 17:18:47 +0200

    Add method to provide trips

--- client-ulysses/src/Service/Provider.ts
+++ client-ulysses/src/Service/Provider.ts
@@ -2,6 +2,8 @@ import UserService from "../domain/use-case/UserService";
 import UserRepository from "../domain/repository/UserRepository"
 import StageService from "../domain/use-case/StageService";
 import StageRepository from "../domain/repository/StageRepository";
+import TripService from "../domain/use-case/TripService";
+import TripRepository from "../domain/repository/TripRepository";
 
 export function ProviderUsers(): UserService{
     const userRepository = new UserRepository();
@@ -15,4 +17,11 @@ export function ProviderStages(): StageService{
     const stageService = new StageService(stageRepository);
 
     return stageService;
+}
+
+export function ProviderTrips(): TripService{
+    const tripRepository = new TripRepository();
+    const tripService = new TripService(tripRepository);
+
+    return tripService;
 }
