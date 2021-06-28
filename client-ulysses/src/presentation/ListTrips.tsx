263b64e307689ea2ed59a224ad073b4ebbeb2ec1
Luisa <luisatrece@gmail.com>
2021-06-28 17:24:29 +0200

    Add react component to list trips

---
+++ client-ulysses/src/presentation/ListTrips.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import * as Provider from "../Service/Provider";
+import Trip from "../domain/entity/Trip";
+
+const ListTrips: React.FC = () =>{
+    const bloc = Provider.ProviderTrips();
+    const [trips, setTrips] = useState(Array<Trip>());
+    bloc.getTrips()
+    .then( res =>{
+        setTrips(res);
+    });
+
+    return(<table>
+        <thead>
+            <tr>
+                <th>PLACE</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                trips.map(
+                    (trip: any) =>
+                    <tr key={trip.id}>
+                        <td>{trip.name}</td>
+                    </tr>
+
+                )
+            }
+        </tbody>
+    </table>);
+}
