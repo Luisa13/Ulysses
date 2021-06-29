165c81415e7f26177208b04997fa42cc8f22dad1
Luisa <luisatrece@gmail.com>
2021-06-29 19:11:09 +0200

    basic login structure

--- client-ulysses/src/Service/ApiService.ts
+++ client-ulysses/src/Service/ApiService.ts
@@ -1,4 +1,24 @@
 
-export default class ApiService{
-    
-}
+const API_BASE_URL = "http://localhost:8080";
+
+export const login = async (username:string, password: string) => {
+    const response = await fetch(`${API_BASE_URL}/auth`, {
+        credentials: 'include',
+        method: 'POST',
+        headers: {
+            ...jsonHeader,
+            ...buildBasicAuthHeader(username , password)
+          }
+    });
+    return response.text();
+};
+
+export const jsonHeader = {
+    'Content-Type': 'application/json;'
+  };
+  
+  export const buildBasicAuthHeader = (username: string, password: string) => {
+    return {
+      'Authorization': 'Basic ' + btoa(username + ':' + password)
+    };
+  };
--- client-ulysses/src/presentation/Login.tsx
+++ client-ulysses/src/presentation/Login.tsx
@@ -1,35 +1,122 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
 //import {useHistory} from 'react-router-dom';
 import {Button, Card, Col, Container, Form, Row} from 'react-bootstrap';
 import {Field, FieldProps, Formik, FormikHelpers, FormikProps} from 'formik';
+import * as Yup from 'yup';
 
+//import {authApi, userApi} from '../api';
+//import {useAppContext} from '../hooks';
+import * as  ApiService from "../Service/ApiService";
 
 type LoginFormFields = {
-    username: string
-    password: string
+  username: string
+  password: string
 }
 
-const Login: React.FC = () =>{
-    return(
-        <React.Fragment>
-      <div className="container">
-        <div className="login-wrapper">
-          <h2>Login Page</h2>
-          <Form className="form-container">
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <Field type="text" name="email" className={"form-control"} placeholder="Email" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <Field type="password" name="password" className={"form-control"} placeholder="Password" />
-            </div>
-            <button type="submit" className="btn btn-primary">Login</button>
-          </Form>
-        </div>
-      </div>
-    </React.Fragment>
-    );
-}
+const Login: React.FC = () => {
+  //const history = useHistory();
+  //const {currentUser, setCurrentUser} = useAppContext();
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const form: {initialValues: LoginFormFields, validationSchema: Yup.AnyObjectSchema} = {
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    validationSchema: Yup.object().shape({
+      username: Yup.string().required('Required'),
+      password: Yup.string().required('Required')
+    })
+  };
+
+  const handleSubmit = async (values: LoginFormFields, formikHelpers: FormikHelpers<LoginFormFields>) => {
+    try {
+      console.log("Login... ");
+      const token = await ApiService.login(values.username, values.password);
+      //localStorage.setItem('token', token);
+      // Get the current user's information next
+      //const user = await userApi.currentUser();
+      // Setting the current user will re-render the routes to be the available in App.tsx
+      formikHelpers.setSubmitting(false);
+     // setCurrentUser(user);
+    } catch (error) {
+      formikHelpers.setSubmitting(false);
+      if (error instanceof TypeError) {
+        setErrorMessage('Unable to connect to service.');
+      } else {
+        setErrorMessage(error.message);
+      }
+    }
+  };
+
+  /*useEffect(() => {
+    if (currentUser) {
+      history.push('/users');
+    }
+  }, [currentUser, history]);*/
+
+  return (
+    <Container fluid className="Login mt-5">
+      <Row>
+        <Col>
+          <Card className="m-auto">
+            <Card.Header className="font-weight-bold text-center">Login</Card.Header>
+            <Card.Body>
+              {errorMessage && <p className="text-center text-danger">{errorMessage}</p>}
+              <Formik initialValues={form.initialValues}
+                      validationSchema={form.validationSchema}
+                      onSubmit={handleSubmit}>
+                {(formikProps: FormikProps<LoginFormFields>) =>
+                  <Form onSubmit={formikProps.handleSubmit}>
+                    <Form.Group controlId="username">
+                      <Form.Label>Username</Form.Label>
+                      <Field name="username">
+                        {({field, meta}: FieldProps) => (
+                          <Form.Control {...field}
+                                        placeholder="Username"
+                                        autoComplete="username"
+                                        isInvalid={!!(meta.touched && meta.error)}/>
+                        )}
+                      </Field>
+                      <Form.Control.Feedback type="invalid">
+                        {formikProps.errors.username}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group controlId="password">
+                      <Form.Label>Password</Form.Label>
+                      <Field name="password">
+                        {({field, meta}: FieldProps) => (
+                          <Form.Control {...field}
+                                        type="password"
+                                        autoComplete="current-password"
+                                        placeholder="Password"
+                                        isInvalid={!!(meta.touched && meta.error)}/>
+                        )}
+                      </Field>
+                      <Form.Control.Feedback type="invalid">
+                        {formikProps.errors.password}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group>
+                      <Button 
+                      variant="primary"
+                      type="submit"
+                              block
+                              disabled={!formikProps.isValid || formikProps.isSubmitting}>
+                        Login
+                      </Button>
+                    </Form.Group>
+                  </Form>
+                }
+              </Formik>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Login;
