0ad7b7e2cd109fa150784750c74952e7defc244f
Luisa <luisatrece@gmail.com>
2021-06-29 19:11:32 +0200

    json package

--- client-ulysses/src/App.tsx
+++ client-ulysses/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import ListUser from './presentation/ListUsers';
+import ListStage from './presentation/ListStages';
+import Login from './presentation/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ListUser/>
+        <Login/>
       </header>
     </div>
   );
