430fc38d0216ea729e0a6e845009eb7cbfc6b593
Luisa <luisatrece@gmail.com>
2021-07-17 20:09:34 +0200

    Changes App component wrapper

--- client-ulysses/src/App.tsx
+++ client-ulysses/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import ListUser from './presentation/ListUsers';
 import ListStage from './presentation/ListStages';
 import Login from './presentation/Login';
@@ -7,9 +9,26 @@ import Login from './presentation/Login';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Login/>
-      </header>
+
+<nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+              </li>
+              <li className="nav-item">
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <header className="App-header"> </header>
+        <div className="outer">
+        <div className="inner">
+          <Login/>
+        </div>
+      </div>
     </div>
   );
 }
