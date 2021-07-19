c0ba7d2c9b6582b0a2593812e0ccbf38a3a332fe
Luisa <luisatrece@gmail.com>
2021-07-19 22:31:29 +0200

    Prevents access to private pages without login

--- client-ulysses/src/App.tsx
+++ client-ulysses/src/App.tsx
@@ -1,35 +1,47 @@
 import React from 'react';
-//import './App.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import ListUser from './presentation/ListUsers';
-import ListStage from './presentation/ListStages';
+//import ListUser from './presentation/ListUsers';
+//import ListStage from './presentation/ListStages';
+import EntryPage from "./presentation/Entry"; //TEMPORAL
 import Login from './presentation/Login';
+import {AuthProvider} from "./domain/components/authContext";
+import {AuthRouteComponent} from "./domain/components/authRoute";
 
 function App() {
   return (
     <div className="App">
 
-<nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-              </li>
-              <li className="nav-item">
-              </li>
-            </ul>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                  </li>
+                  <li className="nav-item">
+                  </li>
+              </ul>
           </div>
         </div>
       </nav>
-
+      
       <header className="App-header"> </header>
-        <div className="outer">
-        <div className="inner">
-          <Login/>
-        </div>
-      </div>
+      <AuthProvider>
+          <Router>
+            <Switch>
+
+              <Route exact path = "/">
+                <Login/>
+              </Route>
+              <AuthRouteComponent exact path = "/Entry">
+                <EntryPage/>
+              </AuthRouteComponent>
+              
+            </Switch>
+          </Router>
+      </AuthProvider>
     </div>
+    
   );
 }
 
