09aa50a3aab9b187aa5cdb4cf135d8cfbc1a32ba
Luisa <luisatrece@gmail.com>
2021-08-08 14:39:05 +0200

    Creates the initial structure for the detail view

--- client-ulysses/src/presentation/detailTripStages.tsx
+++ client-ulysses/src/presentation/detailTripStages.tsx
@@ -1,12 +1,19 @@
-import React, {useState} from 'react'
-import {Button, Col, Container, Card, Row, Carousel} from 'react-bootstrap';
+ import React, {useState} from 'react'
+import {Button, Col, Container, Card, Row} from 'react-bootstrap';
+import ItemCard from './components/itemCardCarousel';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import {Calendar} from 'react-bootstrap-icons';
 
 import { AuthContext } from '../domain/components/authContext';
 
-const DetailTripStages: React.FC = () =>{
+type Props = {
+    stageName: string | "Trip's name"
+}
+const DetailTripStages: React.FC<Props> = ({stageName}) =>{
     const{ userInfo } = React.useContext(AuthContext);
 
-    const [index, setIndex] = useState(0);
+    //const [index, setIndex] = useState(0);
 
     const handleSelect = () => {
         
@@ -14,53 +21,27 @@ const DetailTripStages: React.FC = () =>{
 
     return(
         <Container >
-            <Row><h1>Trip List</h1></Row>
+            <Row><h1>{stageName}</h1></Row>
             <Row className="justify-content-md-center">
-                <Col>
                     <Card>
                         <Card.Body>This is some text within a card body.</Card.Body>
                     </Card>
-
-                    <Carousel activeIndex={index} onSelect={handleSelect}>
-                        <Carousel.Item>
-                            <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=First slide&bg=373940"
-                            alt="First slide"
-                            />
-                            <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=Second slide&bg=282c34"
-                            alt="Second slide"
-                            />
-
-                            <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=Third slide&bg=20232a"
-                            alt="Third slide"
-                            />
-
-                            <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>
-                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                            </p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                    </Carousel>
-                </Col>
+            </Row>
+            <Row >
+                    <AliceCarousel>
+                        <ItemCard
+                            title = "Stage 1"
+                            address = "C/ blablablabla"
+                            telephone = "958 131736"
+                            mail = "example@domain.com"
+                        />
+                        <ItemCard
+                            title = "Stage 2"
+                            address = ""
+                            telephone = ""
+                            mail = "example@domain.com"
+                        />
+                    </AliceCarousel>
             </Row>
         </Container>
     );
