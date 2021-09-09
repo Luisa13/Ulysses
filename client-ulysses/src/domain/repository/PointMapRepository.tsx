9558669329427bbe41182a52e12923fedd22b3ca
Luisa13 <luisatrece@gmail.com>
2021-09-09 11:59:16 +0200

    Adds a respository for pointsMap

---
+++ client-ulysses/src/domain/repository/PointMapRepository.tsx
@@ -0,0 +1,31 @@
+import PointMap from '../entity/PointMap';
+import Stage from '../entity/Stage'
+
+export default class PointMapRepository{
+    baseURL = "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=";
+
+    private async getCoordenates (place: string):Promise<any> {
+        console.log("Looking coordenates for place: " + place)
+        const localURL = this.baseURL + place;
+        const response = await fetch(localURL);
+        const jsonData = await response.json();
+        return jsonData; 
+      }
+      
+      private async getCoordenatesFromAddress (stages: Stage[]):Promise<any[]>{
+        const promises = stages.map((stage) => this.getCoordenates(stage.place));
+        const resp = await Promise.all(promises);
+        return resp
+      }
+      
+      async getPointMaps (stages: Stage[]): Promise<PointMap[]>{
+        const pointArr = [] as PointMap[];
+        const coordinatesObj = await this.getCoordenatesFromAddress(stages);
+        coordinatesObj.forEach(loc =>{
+          const location = loc[0];
+          const point = new PointMap(0, location.display_name, 1, location.lat, location.lon);
+          pointArr.push(point);
+        });
+        return pointArr;
+      } 
+}
