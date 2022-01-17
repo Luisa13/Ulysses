e443f2f0e6ac10ab20c0e9d9a3c3da3cd566a002
Luisa13 <luisatrece@gmail.com>
2022-01-18 00:48:18 +0100

    Add new empty component

---
+++ client-ulysses/src/presentation/components/listNotesStage.tsx
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react'
+import {Button, Col, Container, Card, Row} from 'react-bootstrap';
+
+import Stage from "../../domain/entity/Stage";
+
+
+
+const ListNotesStage: React.FC = () =>{
+
+    return(
+        <Container>
+        </Container>
+    );
+}
+
+export default ListNotesStage;
