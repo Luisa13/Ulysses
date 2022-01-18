ed996d7c8e86983d19d4508fb3a902f724c7df88
Luisa13 <luisatrece@gmail.com>
2022-01-18 12:02:25 +0100

    Update controllers

--- backend-ulysses/src/main/java/com/luisa13/backendulysses/controller/TripController.java
+++ backend-ulysses/src/main/java/com/luisa13/backendulysses/controller/TripController.java
@@ -1,5 +1,6 @@
 package com.luisa13.backendulysses.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,7 +23,7 @@ import com.luisa13.backendulysses.service.TripService;
 import com.luisa13.backendulysses.service.UserService;
 
 @RestController
-@RequestMapping("/trip")
+@RequestMapping("/trip") //Replace for: user/{idUser}/trips
 public class TripController {
 	
 	@Autowired
